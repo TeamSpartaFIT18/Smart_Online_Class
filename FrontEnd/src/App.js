d76b2501fcc48dd69b1854592d7a1c684269b046
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2021-09-21 00:02:25 +0530

    sign up

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginTest from "./Test/Login Test";
 import Home from "./AppHome";
-import SignUp from "./components/SignUp/signUp";
+import RegistrationForm from "./components/SignUp/signUp";
 function App() {
   return (
     <BrowserRouter>
@@ -15,7 +15,7 @@ function App() {
             <LoginTest />
           </Route>
           <Route exact path="/signup">
-            <SignUp />
+          <RegistrationForm />
           </Route>
         </Switch>
       </div>
