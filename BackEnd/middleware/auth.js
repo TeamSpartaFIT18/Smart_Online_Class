0a6fe9c59b983c42a8f3ea7d8efbee4c6efb00f1
Sandun Herath <sandunherath124@gmail.com>
2021-09-22 23:30:22 +0530

    auth middleware and error middleware added

---
+++ BackEnd/middleware/auth.js
@@ -0,0 +1,16 @@
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-fnrh5opn.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https://dev-fnrh5opn.us.auth0.com/api/v2/",
+  issuer: "https://dev-fnrh5opn.us.auth0.com/",
+  algorithms: ["RS256"],
+}).unless({ path: ["/"] });
+
+export default jwtCheck;
