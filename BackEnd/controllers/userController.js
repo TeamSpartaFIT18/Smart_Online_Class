1a8e3b7f71e2d0ab0b73cfdb810c710d87631d74
Sandun Herath <sandunherath124@gmail.com>
2021-09-23 16:38:02 +0530

    user registration done

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -5,31 +5,36 @@ import User from "../models/userModel.js";
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body
-
-  const userExists = await User.findOne({ email })
+  const { name, email, phone_number, picture, user_id } = req.body;
+  console.log(req.body);
+  const userExists = await User.findOne({ email });
 
   if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
+    res.status(400);
+    throw new Error("User already exists");
   }
 
   const user = await User.create({
     name,
     email,
-    password,
-  })
+    phone_number,
+    picture,
+    user_id,
+  });
 
   if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    })
+      phone_number: user.phone_number,
+      auth0_id: user.user_id,
+      picture: user.picture,
+    });
   } else {
-    res.status(400)
-    throw new Error('Invalid user data')
+    res.status(400);
+    throw new Error("Invalid user data");
   }
-})
+});
+
+export { registerUser };
