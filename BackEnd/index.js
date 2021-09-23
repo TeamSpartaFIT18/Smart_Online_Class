450084a291cd98cf4aa30858fb01b1a8962d2774
Sandun Herath <sandunherath124@gmail.com>
2021-09-23 14:54:16 +0530

    mogodb connected

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import bodyParser from "body-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-import jwtCheck from "./middleware/auth"
+import jwtCheck from "./middleware/auth.js";
 dotenv.config();
 
 connectDB();
@@ -10,12 +12,15 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use(jwtCheck);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
