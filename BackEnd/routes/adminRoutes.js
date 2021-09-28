af437975558e745d342344bcc52228a8fb9d9753
Sandun Herath <sandunherath124@gmail.com>
2021-09-28 23:22:12 +0530

    get permition route added

--- BackEnd/routes/adminRoutes.js
+++ BackEnd/routes/adminRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import {getRoles} from "../controllers/adminController.js"
+import { getRoles, getPermissions } from "../controllers/adminController.js";
 import jwtCheck from "../middleware/auth.js";
 
-router.route("/roles")..get(jwtCheck, getRoles);
+router.route("/roles").get(jwtCheck, getRoles);
+router.route("/permissions").get(jwtCheck, getPermissions);
 export default router;
