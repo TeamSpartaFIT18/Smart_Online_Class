0de460035e0c7b9ad3416b5b365a85bb837d4657
Sandun Herath <sandunherath124@gmail.com>
2021-11-04 00:48:56 +0530

    update class completed

--- BackEnd/controllers/adminController.js
+++ BackEnd/controllers/adminController.js
@@ -55,14 +55,15 @@ const getPermissions = asyncHandler(async (req, res) => {
 
 const CreateAClass = asyncHandler(async (req, res) => {
   const { name, subject, category, teacher_email, creator_id } = req.body;
-  const userExists = await Class.findOne({ name });
+  const classExists = await Class.findOne({ name });
   const teacher = await User.findOne({ teacher_email });
   if (teacher == null) {
     res.status(400);
     throw new Error("There is not a teacher with this email !");
   }
   const creator = await User.findById(creator_id);
-  if (userExists) {
+  if (classExists) {
+    //updateClass(req,res);
     res.status(400);
     throw new Error("User already exists");
   }
@@ -129,4 +130,63 @@ const CreateAClass = asyncHandler(async (req, res) => {
   }
 });
 
-export { getRoles, getPermissions, CreateAClass };
+const updateClass = asyncHandler(async (req, res) => {
+  const classExists = await Class.findOne({ name });
+
+  // meeting link
+  //Use the ApiKey and APISecret from config.js
+  const payload = {
+    iss: "MRHFx3gaTx2w0S4MaHwJaQ",
+    exp: new Date().getTime() + 5000,
+  };
+
+  const token = jwt.sign(payload, "Qk5DyjyBym0jnLWYPDGpzqU9CPzlp8UQ51s6");
+  var options = {
+    method: "POST",
+    uri:
+      "https://api.zoom.us/v2/users/" +
+      "sandunherath124@gmail.com" +
+      "/meetings",
+    body: {
+      topic: classExists.name + " " + classExists.subject,
+      type: 1,
+      settings: {
+        host_video: "true",
+        participant_video: "true",
+      },
+    },
+    auth: {
+      bearer: token,
+    },
+    headers: {
+      "User-Agent": "Zoom-api-Jwt-Request",
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    json: true, //Parse the JSON string in the response
+  };
+
+  const result = await axios.post(
+    "https://api.zoom.us/v2/users/" + "sandunherath124@gmail.com" + "/meetings",
+    options.body,
+
+    { headers: options.headers }
+  );
+
+  console.log(result.data.id);
+
+  if (classExists) {
+    classExists.join_url = result.data.join_url;
+    classExists.start_url = result.data.start_url;
+    classExists.meeting_id = result.data.id;
+    classExists.meeting_password = result.data.encrypted_password;
+
+    const classUpdated = await classExists.update();
+    res.json(classUpdated);
+  } else {
+    res.status(404);
+    throw new Error("Class not found");
+  }
+});
+
+export { getRoles, getPermissions, CreateAClass, updateClass };
